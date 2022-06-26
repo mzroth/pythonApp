a009de771d93325344dc55fe5ea1e3723e9c9948
Mark Roth <mzroth@gmail.com>
2022-06-26 00:52:44 -0600

    training

--- app/src/main/java/com/example/pythonapp/MainActivity.kt
+++ app/src/main/java/com/example/pythonapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.pythonapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.pythonapp.ui.theme.PythonAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +25,81 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PythonAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                MessageCard(
+                    Message(
+                        author = "Android",
+                        body = "Jetpack Compose",
+                    )
+                )
+            }
+        }
+    }
+}
+
+data class Message(
+    val author: String,
+    val body: String,
+)
+
+@Composable
+fun MessageCard(message: Message) {
+    PythonAppTheme {
+        Row(
+            modifier = Modifier
+                .padding(all = 8.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = 1.5.dp,
+                        color = MaterialTheme.colors.secondary,
+                        shape = CircleShape,
+                    )
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2,
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                ) {
+                    Text(
+                        text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body2,
+                    )
                 }
             }
         }
     }
 }
 
+@Preview(
+    showBackground = true,
+    name = "Light Mode"
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun PreviewMessageCard() {
     PythonAppTheme {
-        Greeting("Android")
+        MessageCard(
+            message = Message(
+                author = "Colleague",
+                body = "Hey, take a look at Jetpack Compose, it's great!",
+            )
+        )
     }
 }
