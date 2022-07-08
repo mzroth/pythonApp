270cc2c8da4d29aba654f27ca7ae766f7d1e7b47
Mark Roth <mzroth@gmail.com>
2022-07-07 19:13:48 -0600

    added toolbar

--- app/src/main/java/com/example/pythonapp/ui/compose/HomeScreen.kt
+++ app/src/main/java/com/example/pythonapp/ui/compose/HomeScreen.kt
@@ -1,19 +1,35 @@
 package com.example.pythonapp.ui.compose
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pythonapp.R
 import com.example.pythonapp.ui.theme.PythonAppTheme
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Surface(
+    Scaffold(
         modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name))
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "menu"
+                        )
+                    }
+                }
+            )
+        }
     ) {
         Text("Hello world")
     }
