3e16459289e0eb1de7d1d0cc03e80ad35af74cd3
Mark Roth <mzroth@gmail.com>
2022-07-09 23:29:02 -0600

    custom auth working

--- app/src/main/java/com/example/pythonapp/MainActivity.kt
+++ app/src/main/java/com/example/pythonapp/MainActivity.kt
@@ -5,14 +5,61 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.pythonapp.ui.compose.HomeScreen
 import com.example.pythonapp.ui.theme.PythonAppTheme
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : ComponentActivity() {
+    private val signIn = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             PythonAppTheme {
                 HomeScreen()
             }
         }
+
+        val providers = listOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.login_screen)
+            .setGoogleButtonId(R.id.google_sign_in_button)
+            .setEmailButtonId(R.id.email_sign_in_button)
+            .build()
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setTheme(R.style.Theme_PythonApp)
+            .setAuthMethodPickerLayout(customLayout)
+            .build()
+
+        signIn.launch(signInIntent)
     }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
 }
